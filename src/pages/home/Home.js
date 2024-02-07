4e57851aa567cf88745fb99d9ce55d45ac3cca87
Yoana Malinova <110969425+JoanaMalinova@users.noreply.github.com>
2024-02-07 14:35:14 +0200

    added navigation element

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -5,14 +5,23 @@ function Home() {
     return (
         <section className={styles.banner}>
             <img src="images/larger/banner.jpg" alt="witcher-banner" />
-            <div className={styles["heading-wrapper"]}>                
-                <h2 className={styles.smaller}>Welcome to</h2>
-                <h1 className={styles.larger}>The W<span className={styles.red}>i</span>tcher</h1>
-                <h2 className={styles.smaller}>Un<span className={styles.red}>i</span>verse</h2>
-                <blockquote>
-                    <p>But do you know when stories stop being stories? The moment someone begins to believe in them.</p>
-                    <span>-Andzej Sapkowski</span>
-                </blockquote>
+            <div className={styles["right-container"]}>
+                <nav className={styles.navigation}>
+                    <ul role="list">
+                        <li>Games</li>
+                        <li>Series</li>
+                        <li>Books</li>
+                    </ul>
+                </nav>
+                <div className={styles["heading-wrapper"]}>
+                    <h2 className={styles.smaller}>Welcome to</h2>
+                    <h1 className={styles.larger}>The W<span className={styles.red}>i</span>tcher</h1>
+                    <h2 className={styles.smaller}>Un<span className={styles.red}>i</span>verse</h2>
+                    <blockquote>
+                        <p>But do you know when stories stop being stories? The moment someone begins to believe in them.</p>
+                        <span>-Andzej Sapkowski</span>
+                    </blockquote>
+                </div>
             </div>
         </section>
     )
