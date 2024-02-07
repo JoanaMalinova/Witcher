3427336e32bcaad522ee10c3326fbe91d37a344a
Yoana Malinova <110969425+JoanaMalinova@users.noreply.github.com>
2024-02-07 12:45:21 +0200

    added banner heading and changed color to i

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -7,8 +7,8 @@ function Home() {
             <img src="images/larger/banner.jpg" alt="witcher-banner" />
             <div className={styles["heading-wrapper"]}>
                 <h2 className={styles.smaller}>Welcome to</h2>
-                <h1 className={styles.larger}>The Witcher</h1>
-                <h2 className={styles.smaller}>Universe</h2>
+                <h1 className={styles.larger}>The W<span className={styles.red}>i</span>tcher</h1>
+                <h2 className={styles.smaller}>Un<span className={styles.red}>i</span>verse</h2>
             </div>
         </section>
     )
