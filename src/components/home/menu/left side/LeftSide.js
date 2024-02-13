ec6dc3281da1269a530d087a0fb10babb1c2dada
Yoana Malinova <110969425+JoanaMalinova@users.noreply.github.com>
2024-02-13 23:01:28 +0200

    installed react player, made changes to player dimensions

--- src/components/home/menu/left side/LeftSide.js
+++ src/components/home/menu/left side/LeftSide.js
@@ -1,34 +1,16 @@
-import styles from "./LeftSide.module.css"
+import styles from "./LeftSide.module.css";
+import ReactPlayer from "react-player";
 
 function LeftSide({positionStyle}){
 
     return(
         <div className={styles["left-side"]} style={positionStyle}>
-            <iframe
-                width="250"
-                height="200"
-                src="https://www.youtube.com/embed/B00E5f7-yz4"
-            ></iframe>
-             <iframe
-                width="250"
-                height="200"
-                src="https://www.youtube.com/embed/HedLjjlSy3Y"
-            ></iframe>
-             <iframe
-                width="250"
-                height="200"
-                src="https://www.youtube.com/embed/1-l29HlKkXU"
-            ></iframe>
-             <iframe
-                width="250"
-                height="200"
-                src="https://www.youtube.com/embed/MQ2_2Rik3y4"
-            ></iframe>
-             <iframe
-                width="250"
-                height="200"
-                src="https://www.youtube.com/embed/UZP1W6D6aZA"
-            ></iframe>
+            <ReactPlayer
+                width={700}
+                height={500}
+                url="https://www.youtube.com/embed/B00E5f7-yz4"
+                playing={true}
+            />    
         </div>
     )
 }
