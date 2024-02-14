52edc532701ce2a800c73e673501659775cb48f4
Yoana Malinova <110969425+JoanaMalinova@users.noreply.github.com>
2024-02-14 18:05:28 +0200

    added a heading and description to game left side

--- src/components/home/menu/left side/LeftSide.js
+++ src/components/home/menu/left side/LeftSide.js
@@ -1,18 +1,34 @@
 import styles from "./LeftSide.module.css";
 import ReactPlayer from "react-player";
 
-function LeftSide({positionStyle}){
-
-    return(
+function LeftSide({ positionStyle }) {
+    return (
         <div className={styles["left-side"]} style={positionStyle}>
+            <div>
+                <h1>The Witcher</h1>                
+            </div>
+           
             <ReactPlayer
-                width={700}
-                height={500}
+                width={600}
+                height={400}
                 url="https://www.youtube.com/embed/B00E5f7-yz4"
                 playing={true}
-            />    
+            />
+           
+            <div className={styles.description}>
+                <p>
+                    The game is centered around Geralt of Rivia, who is found
+                    unconscious and suffering from amnesia at the beginning of
+                    the game. He gradually learns that he was a renowned
+                    witcher, and that he has friends and enemies almost
+                    everywhere (who do not suffer from amnesia...). Through the
+                    player's actions, he will redefine those relationships and
+                    choose his path in the political intrigue that surrounds
+                    him.
+                </p>
+            </div>
         </div>
-    )
+    );
 }
 
 export default LeftSide;
