832562ea1b3d2587253e667551a9039803cd2caa
Yoana Malinova <110969425+JoanaMalinova@users.noreply.github.com>
2024-02-15 14:44:33 +0200

    changed font to Lato

--- src/components/home/menu/left side/LeftSide.js
+++ src/components/home/menu/left side/LeftSide.js
@@ -4,15 +4,13 @@ import ReactPlayer from "react-player";
 function LeftSide({ positionStyle }) {
     return (
         <div className={styles["left-side"]} style={positionStyle}>
-            <div>
-                <h1>The Witcher</h1>                
-            </div>           
+            <h1>The Witcher</h1>
             <ReactPlayer
                 width={600}
                 height={400}
                 url="https://www.youtube.com/embed/B00E5f7-yz4"
                 playing={true}
-            />           
+            />
             <div className={styles.description}>
                 <p>
                     The game is centered around Geralt of Rivia, who is found
