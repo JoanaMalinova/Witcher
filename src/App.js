e311984e1a9be741da3c85b0916d8b0f96b84c9c
Yoana Malinova <110969425+JoanaMalinova@users.noreply.github.com>
2024-02-16 16:00:32 +0200

    added firebase to project, react router for routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import './App.css';
 import Home from './pages/home/Home';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
 function App() {
-  
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />,
+      children: [
+        {
+          path: ":mediaName",
+          element: <Home />
+        }
+      ]
+    },
+
+  ])
+
   return (
-    <Home/>
+    <RouterProvider router={router}></RouterProvider>
+
   )
 }
 
